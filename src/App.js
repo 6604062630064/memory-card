440154ea1757e72ef435fcaa9418ec6872d4bc56
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-05-12 15:00:30 +0700

    Finish header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+import "./styles/App.css";
 
 function App() {
-	return <div className="App"></div>;
+	const { score, setScore } = useState(0);
+	return (
+		<div className="App">
+			<div className="header">
+				<h1>Memory Game</h1>
+				<div className="status">
+					<h3>Score: 0</h3>
+					<h3>High Score: 0</h3>
+				</div>
+			</div>
+			<div className="content"></div>
+		</div>
+	);
 }
 
 export default App;
